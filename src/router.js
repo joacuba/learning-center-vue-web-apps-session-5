3054ae209c0c3aa64313f9b5d864f19401275d57
joacuba <jqcuba@gmai.com>
2025-09-22 12:09:12 -0500

    feat(in-app-navigation): add page not found component to routes and customize view title.

--- src/router.js
+++ src/router.js
@@ -2,16 +2,25 @@ import Home from "./share/presentation/views/home.vue";
 import {createRouter, createWebHistory} from "vue-router";
 
 const about = () => import("./share/presentation/views/about.vue")
+const pageNotFound = () => import("./share/presentation/views/page-not-found.vue")
 
 const routes = [
     {path: "/home", name: "home", component: Home, meta: {title: "Home"}}, // Eagerly loaded
     {path: "/about", name: "about", component: about, meta: {title: "About"}}, // Lazy loaded
-    {path: "/", redirect: "/home"} // Redirect root to /home
+    {path: "/", redirect: "/home"}, // Redirect root to /home
+    {path: "/:pathMatch(.*)*", name: "not-found", component: pageNotFound, meta: {title: "Page not found"}}, // Lazy loaded
 ];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: routes,
+})
+
+router.beforeEach((to, from, next) => {
+    console.log(`Navigating from ${from.name} to ${to.name}`);
+    let baseTitle = "Acme Learning Center";
+    document.title = `${baseTitle} - ${to.meta['title'] || ''}`;
+    next();
 })
 
 export default router;
