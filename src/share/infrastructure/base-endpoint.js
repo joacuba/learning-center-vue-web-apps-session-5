7f908342e587ee32020f0e1a3025d801cede4736
joacuba <jqcuba@gmail.com>
2025-09-29 10:30:03 -0500

    feat(api): add base-endpoint class for common api operations

---
+++ src/share/infrastructure/base-endpoint.js
@@ -0,0 +1,31 @@
+/**
+ * BaseEndpoint class to handle common API operations
+ * This class can be extended for specific endpoints
+ * It can be extended by specific endpoint classes
+ */
+export class BaseEndpoint {
+    constructor({ baseApi, endpointPath }) {
+        this.http = baseApi.http;
+        this.endpointPath = endpointPath;
+    }
+
+    getAll(){
+       return this.http.get(this.endpointPath);
+    }
+
+    getById(id){
+        return this.http.get(`${this.endpointPath}/${id}`);
+    }
+
+    create(resource){
+        return this.http.post(this.endpointPath, resource);
+    }
+
+    update(id, resource){
+        return this.http.put(`${this.endpointPath}/${id}`, resource);
+    }
+
+    delete(id){
+        return this.http.delete(`${this.endpointPath}/${id}`);
+    }
+}
